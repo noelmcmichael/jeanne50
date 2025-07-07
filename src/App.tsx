2c0bafba8b1364b60b419a4c9496f92f208a3eb2
noelmcmichael <ropak9@gmail.com>
2025-07-06 23:38:13 -0700

    Initial commit: Cleaned up existing project and set up basic structure for Jeannie's birthday website

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,11 @@
-
-import { ThemeProvider } from './contexts/ThemeContext'
-import Navigation from './components/Navigation'
-import Hero from './components/Hero'
-import Features from './components/Features'
-import HowItWorks from './components/HowItWorks'
-import CodeExample from './components/CodeExample'
-import Benefits from './components/Benefits'
-import GetStarted from './components/GetStarted'
-import Footer from './components/Footer'
+import React from 'react';
 
 function App() {
-  console.log('App component rendered')
-  
   return (
-    <ThemeProvider>
-      <div className="min-h-screen bg-gray-50 dark:bg-darkula-700 text-gray-900 dark:text-darkula-100 transition-colors duration-300">
-        {/* Background pattern */}
-        <div className="fixed inset-0 hero-pattern opacity-30 pointer-events-none" />
-        
-        <Navigation />
-        
-        <main className="relative">
-          <Hero />
-          <Features />
-          <HowItWorks />
-          <CodeExample />
-          <Benefits />
-          <GetStarted />
-        </main>
-        
-        <Footer />
-      </div>
-    </ThemeProvider>
-  )
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-gray-900">Jeannie's 50th Birthday Celebration</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
